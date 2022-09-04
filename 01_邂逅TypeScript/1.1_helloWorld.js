aa95d43436b1a59f4f6ed5b156c776879a2c5632
leslieXin92 <leslieXin92@gmail.com>
2022-09-04 16:29:23 +0800

    feat: tsc hello world

---
+++ 01_邂逅TypeScript/1.1_helloWorld.js
@@ -0,0 +1,7 @@
+var msg = 'hello world';
+// msg = 123
+function showMsgLen(msg) {
+    console.log(msg.length);
+}
+// showMsgLen(123)
+showMsgLen();
---
+++ 01_邂逅TypeScript/1.1_helloWorld.ts
@@ -0,0 +1,8 @@
+let msg: string = 'hello world'
+// msg = 123
+
+function showMsgLen(msg: string) {
+	console.log(msg.length)
+}
+// showMsgLen(123)
+// showMsgLen()
