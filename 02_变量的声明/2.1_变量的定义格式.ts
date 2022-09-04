887b7ab8ad906e1a9f400a33a2c1b0b0ac42714f
leslieXin92 <leslieXin92@gmail.com>
2022-09-04 17:49:27 +0800

    feat: 变量的声明

---
+++ 02_变量的声明/2.1_变量的定义格式.ts
@@ -0,0 +1,9 @@
+var name: string = 'leslie'
+let age: number = 24
+const lover: string = 'cabbage'
+const msg = 'hello TypeScript' // 默认情况下赋值，会将值的类型赋给变量（类型推导）
+
+// string => TypeScript中的字符串类型
+// String => JavaScript的字符串包装类的类型
+
+export {}
