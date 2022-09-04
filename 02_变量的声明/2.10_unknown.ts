876feac0fff3228fcd1bcf419866636f2d1607d3
leslieXin92 <leslieXin92@gmail.com>
2022-09-04 19:18:00 +0800

    feat: 补充any、unknown类型

---
+++ 02_变量的声明/2.10_unknown.ts
@@ -0,0 +1,18 @@
+function getName() {
+	return 'leslie'
+}
+
+function getAge() {
+	return 24
+}
+
+const flag = Math.random() * 10 > 5
+
+// unknown类型只能赋值给any和unknown类型，为了防止unknown类型的变量乱给别人赋值
+let res: unknown
+
+if (flag) {
+	res = getName()
+} else {
+	res = getAge()
+}
---
+++ 02_变量的声明/2.9_any.ts
@@ -0,0 +1,4 @@
+let msg: any = 'hello world'
+msg = 111
+msg = true
+msg = {}
