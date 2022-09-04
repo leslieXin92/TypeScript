d71559a0eff9c945c9ff8238169c5b71ed3067ce
leslieXin92 <leslieXin92@gmail.com>
2022-09-05 01:15:54 +0800

    feat: 非空类型断言

--- 03_函数的声明/3.7_类型断言.ts
+++ 03_函数的声明/3.7_类型断言.ts
@@ -1,6 +1,10 @@
 const imgEl = document.querySelector('img') as HTMLImageElement
-
 imgEl.src = ''
 
 const msg = 'hello world'
 const num: number = msg as any as number // 不推荐这么搞
+
+// 非空类型断言
+function logMsgLen(msg?: string) {
+	console.log(msg!.length)
+}
