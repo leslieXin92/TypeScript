9eb3750c4a78e5d57af7ae9d3b3bf98848349b7b
leslieXin92 <leslieXin92@gmail.com>
2022-09-06 00:22:08 +0800

    feat: class抽象类

---
+++ 06_类/6.8_抽象类.ts
@@ -0,0 +1,40 @@
+function calcArea(shape: Shape) {
+	return shape.getArea()
+}
+
+abstract class Shape {
+	abstract getArea() // 抽象函数不能有函数体
+}
+
+class Rectangle extends Shape {
+	constructor(width: number, height: number) {
+		super()
+		this.width = width
+		this.height = height
+	}
+	private width: number = 0
+	private height: number = 0
+	// 抽象类的方法必须由子类实现
+	getArea() {
+		return this.width * this.height
+	}
+}
+
+class Circle extends Shape {
+	constructor(radius: number) {
+		super()
+		this.radius = radius
+	}
+	private radius: number = 0
+	// 抽象类的方法必须由子类实现
+	getArea() {
+		return this.radius * this.radius * Math.PI
+	}
+}
+
+const rectangle = new Rectangle(4, 5)
+const circle = new Circle(2)
+
+console.log(calcArea(rectangle))
+console.log(calcArea(circle))
+// console.log(new Shape()) // 使用抽象类就是为了解决这种情况
