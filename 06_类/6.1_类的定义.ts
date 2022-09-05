b4d33a31e0401ff85172f8eaac82b130fac14a2c
leslieXin92 <leslieXin92@gmail.com>
2022-09-05 20:28:15 +0800

    feat: class基本使用

---
+++ 06_类/6.1_类的定义.ts
@@ -0,0 +1,17 @@
+class Person {
+	constructor(name: string, age: number) {
+		this.name = name
+		this.age = age
+	}
+	name: string = ''
+	age: number = 0
+	eating() {
+		console.log(`${this.name} - eating`)
+	}
+}
+
+const p = new Person('leslie', 24)
+console.log(p) // { name: 'leslie', age: 24}
+p.eating() // leslie - eating
+
+export {}
