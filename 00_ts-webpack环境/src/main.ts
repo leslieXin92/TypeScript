eab2557b3fb3f5d916f18ab3309c0e5919f44ff5
leslieXin92 <leslieXin92@gmail.com>
2022-09-07 22:54:50 +0800

    feat: ts-webpack环境

---
+++ 00_ts-webpack环境/src/main.ts
@@ -0,0 +1,2 @@
+const msg: string = 'leslie'
+console.log(msg)
---
+++ 00_ts-webpack环境/webpack.config.js
@@ -0,0 +1,23 @@
+const HtmlWebpackPlugin = require('html-webpack-plugin')
+const path = require('path')
+
+module.exports = {
+	mode: 'development',
+	entry: './src/main.ts',
+	output: {
+		path: path.resolve(__dirname, './dist'),
+		filename: 'bundle.js'
+	},
+	resolve: {
+		extensions: ['.ts', '.js', '.json']
+	},
+	module: {
+		rules: [
+			{
+				test: /\.ts$/,
+				loader: 'ts-loader'
+			}
+		]
+	},
+	plugins: [new HtmlWebpackPlugin({ template: './index.html' })]
+}
